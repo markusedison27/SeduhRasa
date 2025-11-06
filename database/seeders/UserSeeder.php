5caa7948bbd4ca132bb3c61f00159ef2bcaae53f
arif <arifinilhamjuli24@gmail.com>
2025-11-06 21:26:36 +0700

    menambahkan akun untuk masuk dashboard

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,18 +5,50 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class UserSeeder extends Seeder
 {
-    public function run(): void
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Admin',
-            'email' => 'admin@seduhrasa.com', // Email untuk login
-            'password' => Hash::make('password123'), // Password Anda adalah 'password123'
-            'email_verified_at' => now(),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // Data 4 akun admin/pemilik
+        $users = [
+            [
+                'name' => 'Super Admin',
+                'email' => 'admin@seduhrasa.com',
+                'password' => Hash::make('password123'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Pemilik 1 (Kasir)',
+                'email' => 'kasir1@seduhrasa.com',
+                'password' => Hash::make('kasir123'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Pemilik 2 (Manajer)',
+                'email' => 'manager@seduhrasa.com',
+                'password' => Hash::make('manager123'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Pemilik 3 (Barista)',
+                'email' => 'barista@seduhrasa.com',
+                'password' => Hash::make('barista123'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ];
+
+        // Masukkan data ke tabel 'users' (atau 'pemilik', sesuaikan Model Anda)
+        DB::table('users')->insert($users);
     }
 }
