2204670274ce18a768ed4af0cd307c883f9e8766
MarkusEdison <markusedison4gmail.com>
2025-12-16 09:26:28 +0700

    tes

--- app/Models/orderitem.php
+++ app/Models/orderitem.php
@@ -2,12 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OrderItem extends Model
 {
-    use HasFactory;
+     use HasFactory;
 
     protected $fillable = [
         'order_id',
